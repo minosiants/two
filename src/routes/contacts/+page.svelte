9044c0413e1ce56879835a63c41725d18e817aba
minosiants <kaspar.ru@gmail.com>
2025-07-28 12:44:15 +1000

    Add card

--- src/routes/contacts/+page.svelte
+++ src/routes/contacts/+page.svelte
@@ -2,64 +2,7 @@
   import Contacts from "$lib/components/Contacts.svelte";
   import Search from "$lib/components/Search.svelte";
   import { setContext } from "svelte";
-  const _contacts = [
-    {
-      id: 1,
-      photo: "",
-      selected: false,
-      name: "a Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 2,
-      photo: "",
-      selected: false,
-      name: "d Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 3,
-      photo: "",
-      selected: false,
-      name: "c Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 4,
-      photo: "",
-      selected: false,
-      name: "Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 5,
-      photo: "",
-      selected: false,
-      name: "Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 6,
-      photo: "",
-      selected: false,
-      name: "Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 7,
-      photo: "",
-      selected: false,
-      name: "Kaspar Minosyants",
-      contact: "+71234567",
-    },
-    {
-      id: 8,
-      photo: "",
-      selected: false,
-      name: "Kaspar Minosyants",
-      contact: "+71234567",
-    },
-  ];
+  import _contacts from "$lib/js/contacts";
   let contacts = $state(_contacts);
   setContext("contacts", contacts);
 </script>
